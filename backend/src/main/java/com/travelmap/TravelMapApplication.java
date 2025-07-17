d8fd5fd68e40ae43212d85eed912cf10ea6d8011
Yi da-som <somlh1212@gmail.com>
2025-07-17 23:28:35 +0900

    move the package

--- backend/src/main/java/com/travelmap/TravelMapApplication.java
+++ backend/src/main/java/com/travelmap/TravelMapApplication.java
@@ -1,4 +1,4 @@
-package com.travelmap.backend;
+package com.travelmap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/travelmap/TravelMapApplicationTests.java
+++ backend/src/test/java/com/travelmap/TravelMapApplicationTests.java
@@ -1,4 +1,4 @@
-package com.travelmap.backend;
+package com.travelmap;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
